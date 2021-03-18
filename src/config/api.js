b0cc9a368310ce914124b9b07c363dc656589fef
Jan Siegl <42580612+jan-siegl@users.noreply.github.com>
2021-03-18 13:50:55 +0100

    Hide API key

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 //Mapbox api
-export const mapboxKey = "pk.eyJ1Ijoic2llZ2xqIiwiYSI6ImNrbWUxbHJqcjFqcWsybnFscGJoczVmajEifQ.JFn8aFgu8cIBRR6wSGe_pw"
+export const mapboxKey = process.env.REACT_APP_MAP_API_KEY
 
 //initial map location and zoom, set to Prague center
 export const initialMap = {
@@ -12,7 +12,7 @@ export const initialMap = {
 
 
 //golemio opendata api
-export const golemioKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imphbi5zaWVnbEBwbS5tZSIsImlkIjo2NjcsIm5hbWUiOm51bGwsInN1cm5hbWUiOm51bGwsImlhdCI6MTYxNjAyMzUxMCwiZXhwIjoxMTYxNjAyMzUxMCwiaXNzIjoiZ29sZW1pbyIsImp0aSI6IjIxM2UyOWQ1LTY3MzctNGQ4YS05MjliLTk0ZDM4MDVlYmVkMyJ9.CJZ-ahmi-WkPvVw7BJFA3xfn6ps70UDqaONP4oVKsrs"
+export const golemioKey = process.env.REACT_APP_GOLEMIO_API_KEY
 
 //specific line location query
 export const locationQuery = "https://api.golemio.cz/v2/vehiclepositions?includeNotTracking=false&routeShortName="
